ff22f49bcd73c1543a783297747417f10f93392e
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2025-01-03 17:52:53 +0530

    Create tapas.sh

---
+++ tapas.sh
@@ -0,0 +1,5 @@
+yum install httpd -y
+systemctl enable httpd --now
+tar -xvf /root/cp200/SingleContainerFile.tar 
+cp -rvf /root/cp200/SingleContainerFile/Containerfile /var/www/html/
+systemctl restarted httpd 
