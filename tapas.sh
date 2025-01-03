a9348c84ac9375d7e9a4486c00bdfed95b5f3850
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2025-01-03 17:58:27 +0530

    Update with user name tapas.sh

--- tapas.sh
+++ tapas.sh
@@ -3,3 +3,5 @@ systemctl enable httpd --now
 tar -xvf /root/cp200/SingleContainerFile.tar 
 cp -rvf /root/cp200/SingleContainerFile/Containerfile /var/www/html/
 systemctl restarted httpd 
+useradd alth
+echo redhat | passwd --stdin alth
