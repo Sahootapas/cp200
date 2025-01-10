42d65adb09337f08aa012ceac252f04d0fedca69
Tapas kumar sahoo <166196075+Sahootapas@users.noreply.github.com>
2025-01-10 13:03:33 +0530

    Update tapas.sh

--- tapas.sh
+++ tapas.sh
@@ -2,6 +2,6 @@ yum install httpd -y
 systemctl enable httpd --now
 tar -xvf /root/cp200/SingleContainerFile.tar 
 cp -rvf /root/cp200/SingleContainerFile/Containerfile /var/www/html/
-systemctl restarted httpd 
+systemctl restart httpd 
 useradd alth
 echo redhat | passwd --stdin alth
